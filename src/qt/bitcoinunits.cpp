e33021cc06c3d3f08757393246b2a2e4eb04c045
Rafael Martinez Marconi - COGTI/CIMGC/CICSM <81205635572@serpro-1549746.(none)>
2014-02-27 17:43:07 -0300

    LTC to KLC

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("KLC");
+    case mBTC: return QString("mKLC");
+    case uBTC: return QString::fromUtf8("μKLC");
     default: return QString("???");
     }
 }
